e0469b8d386cd95f97b768755c20f1bde1869e52
GoelVaibhav009 <creativeprogrammer623@gmail.com>
2021-01-22 03:08:15 +0530

    Login page Ui created

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,15 @@ const router = express.Router()
 // @desc   Login/Landing page
 // @route  GET /
 router.get('/', (req, res) => {
-    res.send('Login')
+    res.render('login', {
+        layout: 'login'
+    })
 })
 
 // @desc   Dashboard
 // @route  GET /dashboard
 router.get('/dashboard', (req, res) => {
-    res.send('Dashboard')
+    res.render('dashboard')
 })
 
 module.exports = router
