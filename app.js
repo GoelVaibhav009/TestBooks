abc58e6984e81d5302cb866ae72710f703bc6e91
GoelVaibhav009 <creativeprogrammer623@gmail.com>
2021-01-23 17:11:35 +0530

    Google OAUTH2.0

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const express =  require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
+const connectDB = require('./config/db')
 
 //Load config
 dotenv.config({ path: 'config/config.env'})
-const connectDB = require('./config/db')
 
+//Passport Config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -22,11 +26,23 @@ if (process.env.NODE_ENV === 'development') {
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', '.hbs')
 
+//Sessions
+app.use(session({
+    secret: 'Hello',
+    resave: false,
+    saveUninitialized: false,
+}))
+
+//Passport middlewares
+app.use(passport.initialize())
+app.use(passport.session())
+
 // Static folder
 app.use(express.static(path.join(__dirname, 'public'))) 
 
 // Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 5000
 
