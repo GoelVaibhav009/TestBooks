d7a7916c43a469d92ed7da6c1addba08ee92520b
GoelVaibhav009 <creativeprogrammer623@gmail.com>
2021-01-23 18:39:53 +0530

    Add Stories

--- app.js
+++ app.js
@@ -19,13 +19,20 @@ connectDB()
 
 const app = express()
 
+// Body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
 // Logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
+//Handlebars Helpers
+const { formatDate } = require('./helpers/hbs')
+
 //Handlesbars
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.engine('.hbs', exphbs({ helpers: {formatDate}, defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', '.hbs')
 
 //Sessions
@@ -46,6 +53,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 5000
 
