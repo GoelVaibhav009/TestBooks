0fc3413c9a9a19c7f016055730052a3ba877c1e4
GoelVaibhav009 <creativeprogrammer623@gmail.com>
2021-01-24 13:47:59 +0530

    Added User Stories

--- routes/stories.js
+++ routes/stories.js
@@ -136,4 +136,25 @@ router.delete('/:id', ensureAuth, async (req, res) => {
     return res.render('error/500')
   }
 })
+
+// @desc    User stories
+// @route   GET /stories/user/:userId
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({
+      user: req.params.userId,
+      status: 'public',
+    })
+      .populate('user')
+      .lean()
+
+    res.render('stories/index', {
+      stories,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+})
+
 module.exports = router;
